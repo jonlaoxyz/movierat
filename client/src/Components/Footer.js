30799f570b171bd664b8b18f4e0073097103b2fa
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2024-02-06 23:53:53 -0600

    different components created and main.js (updated app.js)

---
+++ client/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="Footer">
+        <p>movieRAT! &#169; 2024 LHL Finals Project</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
