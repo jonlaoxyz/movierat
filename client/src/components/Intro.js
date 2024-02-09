369e96d8da5d76a89b45bd946849c5758b6a757e
Jonathan Lao <jon.lao@gmail.com>
2024-02-09 17:27:27 -0600

    minor change to Intro.js

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -15,7 +15,7 @@ class Intro extends React.Component {
                 Simplify your movie-watching experience with movieRAT!</p>
             </div>
             <div className="col-md-4 text-center text-md-center mt-3 mt-md-0">
-              <img src="./img/movierat-mascot.jpg" className="rounded-circle img-fluid" style={{ maxWidth: "350px" }} />
+              <img src="./img/movierat-mascot.jpg" className="rounded-circle img-fluid" style={{ maxWidth: "350px" }} alt="movieRAT! mascot" />
             </div>
           </div>
         </div>
